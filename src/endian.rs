fa06b5ef62d3d91ad1537a3ee0e2d591d1b2b9e6
lake <102535481@qq.com>
2025-11-10 22:36:46 +0800

    优化

--- src/endian.rs
+++ src/endian.rs
@@ -1,4 +1,12 @@
+#[cfg(not(feature = "bin"))]
 #[derive(Debug, Clone, Eq, PartialEq)]
+#[allow(dead_code)]
+pub enum Endian {
+    Big,
+    Little,
+}
+#[cfg(feature = "bin")]
+#[derive(bincode::Encode, bincode::Decode, Debug, Clone, Eq, PartialEq)]
 #[allow(dead_code)]
 pub enum Endian {
     Big,
